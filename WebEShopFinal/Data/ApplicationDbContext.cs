4485002f45260349e37971b37fe51d03c5b437e2
BaStath007 <yiannis_mar@outlook.com>
2023-01-12 18:07:35 +0200

    Add project files.

---
+++ WebEShopFinal/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebEShopFinal.Models;
+
+namespace WebEShopFinal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<ApplicationUser> Users { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+}
