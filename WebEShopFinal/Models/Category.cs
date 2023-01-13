b199b98707f44bc8827be1bdc294e8843137af81
BaStath007 <yiannis_mar@outlook.com>
2023-01-13 17:19:54 +0200

    messing around, tottaly confused

--- WebEShopFinal/Models/Category.cs
+++ WebEShopFinal/Models/Category.cs
@@ -9,13 +9,13 @@ namespace WebEShopFinal.Models
 {
     public class Category : IDisposable
     {
+        [DisplayName("Category Id")]
         public int Id { get; set; } // Id (Primary key)
-
-        [DisplayName("Product Title")]
+        
         [Required]
         public string Title { get; set; } // Title
         public string Description { get; set; } // Description
-        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<Product>? Products { get; set; }
 
         public void Dispose()
         {
